838b6e07d533de27c714b1a0cc94c05f8d02a3af
Amine_Lab <labiodh.amine@gmail.com>
2022-09-03 12:43:34 +0100

    interceptor

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private host: string = environment.apiUrl;
+  private token !: string ;
+  private loggedInUsername !: string ;
+  private jwtHelper = new JwtHelperService();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>
+    (`${this.host}/user/login`, user, {observe: 'response'});
+  }
+
+  public register(user: User): Observable<User | HttpErrorResponse> {
+    return this.http.post<User | HttpErrorResponse>
+    (`${this.host}/user/register`, user);
+  }
+
+  public logOut():void {
+   this.token = "";
+   this.loggedInUsername = "";
+   localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('users');
+  }
+
+  public saveToken(token : string):void {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
+  public addUserToLocalCash(user : User):void {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+  public getUserFromLocalCash():User {
+    // @ts-ignore
+    return JSON.parse(localStorage.getItem('user')) ;
+  }
+  public loadToken():void {
+    // @ts-ignore
+    this.token = localStorage.getItem('token');
+  }
+  public getToken():string {
+    return this.token ;
+  }
+  // @ts-ignore
+  public isLoggedIn():boolean {
+    this.loadToken();
+    if (this.token != null && this.token !== ''){
+      if (this.jwtHelper.decodeToken(this.token).sub != null || ''){
+        if (this.jwtHelper.isTokenExpired(this.token)){
+          this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+          return true;
+        }
+      }
+    }else{
+      this.logOut();
+      return false;
+    }
+
+  }
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpErrorResponse, HttpEvent, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {CustomHttpRespone} from "../model/custom-http-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private host: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.host}/user/list`);
+  }
+
+  public addUser(formData: FormData): Observable<User> {
+    return this.http.post<User>(`${this.host}/user/add`, formData);
+  }
+
+  public updateUser(formData: FormData): Observable<User> {
+    return this.http.post<User>(`${this.host}/user/update`, formData);
+  }
+
+  public resetPassword(email: string): Observable<CustomHttpRespone> {
+    return this.http.get<CustomHttpRespone>(`${this.host}/user/resetpassword/${email}`);
+  }
+
+  public updateProfileImage(formData: FormData): Observable<HttpEvent<User>> {
+    return this.http.post<User>(`${this.host}/user/updateProfileImage`, formData,
+      {reportProgress: true,
+        observe: 'events'
+      });
+  }
+
+  public deleteUser(username: string): Observable<CustomHttpRespone> {
+    return this.http.delete<CustomHttpRespone>(`${this.host}/user/delete/${username}`);
+  }
+
+  public addUsersToLocalCache(users: User[]): void {
+    localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  public getUsersFromLocalCache(): User[] | null {
+    if (localStorage.getItem('users')) {
+      // @ts-ignore
+      return JSON.parse(localStorage.getItem('users'));
+    }
+    return null;
+  }
+
+  public createUserFormDate(loggedInUsername: string, user: User, profileImage: File): FormData {
+    const formData = new FormData();
+    formData.append('currentUsername', loggedInUsername);
+    formData.append('firstName', user.firstName);
+    formData.append('lastName', user.lastName);
+    formData.append('username', user.username);
+    formData.append('email', user.email);
+    formData.append('role', user.role);
+    formData.append('profileImage', profileImage);
+    formData.append('isActive', JSON.stringify(user.active));
+    formData.append('isNonLocked', JSON.stringify(user.notLocked));
+    return formData;
+  }
+}
