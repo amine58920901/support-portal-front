5cf2c1c48399274c785a6eb41ca1f70bd5a165f7
Amine_Lab <labiodh.amine@gmail.com>
2022-09-07 15:33:04 +0100

    login template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AuthenticationService} from "./service/authentication.service";
+import {UserService} from "./service/user.service";
+import {AuthInterceptor} from "./interceptor/auth.interceptor";
+import {AuthenticationGuard} from "./guard/authentication.guard";
+import {NotificationModule} from "./notification.module";
+import {NotificationService} from "./service/notification.service";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NotificationModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NotificationService,AuthenticationGuard,AuthenticationService, UserService, {provide: HTTP_INTERCEPTORS,useClass:AuthInterceptor , multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -9,65 +9,68 @@ import {JwtHelperService} from "@auth0/angular-jwt";
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private host: string = environment.apiUrl;
-  private token !: string ;
-  private loggedInUsername !: string ;
+  public host = environment.apiUrl;
+  private token!: string;
+  private loggedInUsername !: string;
   private jwtHelper = new JwtHelperService();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+
+  public login(user: User): Observable<HttpResponse<User>> {
+    return this.http.post<User>(`${this.host}/user/login`, user, { observe: 'response' });
   }
 
-  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
-    return this.http.post<HttpResponse<any> | HttpErrorResponse>
-    (`${this.host}/user/login`, user, {observe: 'response'});
+  public register(user: User): Observable<User> {
+    return this.http.post<User>(`${this.host}/user/register`, user);
   }
 
-  public register(user: User): Observable<User | HttpErrorResponse> {
-    return this.http.post<User | HttpErrorResponse>
-    (`${this.host}/user/register`, user);
-  }
-
-  public logOut():void {
-   this.token = "";
-   this.loggedInUsername = "";
-   localStorage.removeItem('user');
+  public logOut(): void {
+    // @ts-ignore
+    this.token = null;
+    // @ts-ignore
+    this.loggedInUsername = null;
+    localStorage.removeItem('user');
     localStorage.removeItem('token');
     localStorage.removeItem('users');
   }
 
-  public saveToken(token : string):void {
+  public saveToken(token: string): void {
     this.token = token;
     localStorage.setItem('token', token);
   }
 
-  public addUserToLocalCash(user : User):void {
+  public addUserToLocalCache(user: User): void {
     localStorage.setItem('user', JSON.stringify(user));
   }
-  public getUserFromLocalCash():User {
+
+  public getUserFromLocalCache(): User {
     // @ts-ignore
-    return JSON.parse(localStorage.getItem('user')) ;
+    return JSON.parse(localStorage.getItem('user'));
   }
-  public loadToken():void {
+
+  public loadToken(): void {
     // @ts-ignore
     this.token = localStorage.getItem('token');
   }
-  public getToken():string {
-    return this.token ;
+
+  public getToken(): string {
+    return this.token;
   }
+
   // @ts-ignore
-  public isLoggedIn():boolean {
+  public isUserLoggedIn(): boolean {
     this.loadToken();
     if (this.token != null && this.token !== ''){
-      if (this.jwtHelper.decodeToken(this.token).sub != null || ''){
-        if (this.jwtHelper.isTokenExpired(this.token)){
+      if (this.jwtHelper.decodeToken(this.token).sub != null || '') {
+        if (!this.jwtHelper.isTokenExpired(this.token)) {
           this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
           return true;
         }
       }
-    }else{
+    } else {
       this.logOut();
       return false;
     }
-
   }
+
 }
